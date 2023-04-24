37771b5a4fa5ceffb74321165e2d1ca59eb05ed4
hami9m72 <21522332@gm.uit.edu.vn>
2023-04-24 21:58:26 +0700

    add data sql - destination for combobox

--- Quan-Ly-Ban-Ve-May-Bay/MainWindow.xaml.cs
+++ Quan-Ly-Ban-Ve-May-Bay/MainWindow.xaml.cs
@@ -14,23 +14,68 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Quan_Ly_Ban_Ve_May_Bay
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
-    { 
+    {
+        static string strConnection = @"Data Source=LAPTOP-68IV7KVK\SQLEXPRESS;Initial Catalog=Data;Integrated Security=True";
+        SqlConnection sqlConnection = new SqlConnection(strConnection);
+        SqlCommand sqlCommand = new SqlCommand();
+        SqlDataAdapter adapter;
+        DataSet ds;
+        SqlDataReader reader;
         public MainWindow()
         {
             InitializeComponent();
+            bindcombo();
         }
 
+        public List<destination> des { get; set; }
+
+        private void bindcombo()
+        {
+            sqlConnection.Open();
+            sqlCommand = new SqlCommand("select * from ccb_destination", sqlConnection);
+            adapter = new SqlDataAdapter(sqlCommand);
+            ds = new DataSet();
+            adapter.Fill(ds, "ccb_destination");
+            destination des = new destination();
+            IList<destination> destinations = new List<destination>();
+
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+                destinations.Add(new destination
+                {
+                    code = dr[0].ToString(),
+                    city = dr[1].ToString()
+                });
+            }
+            ds = null;
+            adapter.Dispose();
+            sqlConnection.Close();
+            sqlConnection.Dispose();
+            cbb_from.ItemsSource = destinations;
+            cbb_from.DisplayMemberPath = "city";
+            cbb_to.ItemsSource = destinations;
+            cbb_to.DisplayMemberPath = "city";
+
+        }
+
+        
         private void btnSearch_Click(object sender, RoutedEventArgs e)
         {
             FlightsList flightWindow = new FlightsList();
             flightWindow.Show();
+        }
+
+        public class destination
+        {
+            public string code { get; set; }
+            public string city { get; set; }
         }
     }
 }
