9ed0096805426690edccdad5ed61ac11340c312e
hami9m72 <21522332@gm.uit.edu.vn>
2023-04-28 18:56:35 +0700

    delete ex-menutab

--- Quan-Ly-Ban-Ve-May-Bay/MainWindow.xaml.cs
+++ Quan-Ly-Ban-Ve-May-Bay/MainWindow.xaml.cs
@@ -22,57 +22,11 @@ namespace Quan_Ly_Ban_Ve_May_Bay
 
     public partial class MainWindow : Window
     {
-        static string strConnection = @"Data Source=LAPTOP-68IV7KVK\SQLEXPRESS;Initial Catalog=Data;Integrated Security=True";
-        SqlConnection sqlConnection = new SqlConnection(strConnection);
-        SqlCommand sqlCommand = new SqlCommand();
-        SqlDataAdapter adapter;
-        DataSet ds;
-        SqlDataReader reader;
         public MainWindow()
         {
             InitializeComponent();
-            bindcombo();
+            
         }
-
-        public List<destination> des { get; set; }
-
-        private void bindcombo()
-        {
-            sqlConnection.Open();
-            sqlCommand = new SqlCommand("select * from ccb_destination", sqlConnection);
-            adapter = new SqlDataAdapter(sqlCommand);
-            ds = new DataSet();
-            adapter.Fill(ds, "ccb_destination");
-            destination des = new destination();
-            IList<destination> destinations = new List<destination>();
-
-            foreach (DataRow dr in ds.Tables[0].Rows)
-            {
-                destinations.Add(new destination
-                {
-                    code = dr[0].ToString(),
-                    city = dr[1].ToString()
-                });
-            }
-            ds = null;
-            adapter.Dispose();
-            sqlConnection.Close();
-            sqlConnection.Dispose();
-            //cbb_from.ItemsSource = destinations;
-            //cbb_from.DisplayMemberPath = "city";
-            //cbb_to.ItemsSource = destinations;
-            //cbb_to.DisplayMemberPath = "city";
-
-        }
-
-        
-        
-        public class destination
-        {
-            public string code { get; set; }
-            public string city { get; set; }
-        }
-
         private void btnHome_Click(object sender, RoutedEventArgs e)
         {
             fContainer.Navigate(new System.Uri("Pages/Home.xaml", UriKind.RelativeOrAbsolute));
--- Quan-Ly-Ban-Ve-May-Bay/Pages/Home.xaml.cs
+++ Quan-Ly-Ban-Ve-May-Bay/Pages/Home.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static Quan_Ly_Ban_Ve_May_Bay.MainWindow;
 
 namespace Quan_Ly_Ban_Ve_May_Bay.Pages
 {
@@ -20,15 +23,52 @@ namespace Quan_Ly_Ban_Ve_May_Bay.Pages
     /// </summary>
     public partial class Home : Page
     {
+        /* SqlConnection sqlConnection = new SqlConnection(@"Server=(local);Database=Data;Trusted_Connection=Yes;");
+        SqlCommand sqlCommand = new SqlCommand();
+        SqlDataAdapter adapter;
+        DataSet ds; */
         public Home()
         {
             InitializeComponent();
+            //bindcombobox();
         }
         private void btnSearch_Click(object sender, RoutedEventArgs e)
         {
             FlightsList flightWindow = new FlightsList();
             flightWindow.Show();
         }
+        /*private void bindcombobox()
+        {
+            sqlConnection.Open();
+            sqlCommand = new SqlCommand("select * from ccb_destination", sqlConnection);
+            adapter = new SqlDataAdapter(sqlCommand);
+            ds = new DataSet();
+            adapter.Fill(ds, "ccb_destination");
+            IList<destination> destinations = new List<destination>();
+
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+                destinations.Add(new destination
+                {
+                    code = dr[0].ToString(),
+                    city = dr[1].ToString()
+                });
+            }
+            ds = null;
+            adapter.Dispose();
+            sqlConnection.Close();
+            sqlConnection.Dispose();
+            cbb_from.ItemsSource = destinations;
+            cbb_from.DisplayMemberPath = "city";
+            cbb_to.ItemsSource = destinations;
+            cbb_to.DisplayMemberPath = "city";
+
+        }
+        public class destination
+        {
+            public string code { get; set; }
+            public string city { get; set; }
+        }*/
 
     }
 }
